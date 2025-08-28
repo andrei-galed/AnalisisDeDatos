da653b681806fc13060fa96998a6f4057dd9a6ef
andrei-galed <andreigaled3@gmail.com>
2025-08-28 19:16:19 +0000

    Sesion2.1

--- Sesion2/Docker2/app.py
+++ Sesion2/Docker2/app.py
@@ -1,14 +1,17 @@
-#  pip install Flask==2.2.5
+#  pip install Flask==2.2.5 
+#Instala una libreria para poder desplegar desarrollos web .py
 #  pip install Werkzeug==2.2.3
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__)  #Con este creo el servico Flask que se va a llamar app
 
 @app.route('/api/hello')
+
 def hello():
-    return 'Hola Mundo desde Docker'
+    return 'hola mundo desde docker'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    
+     app.run(host='0.0.0.0', port=5000) 
+
+ #cd .. te lleva a la carpeta de atras 
